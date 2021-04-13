09dd16d64f31315c637dd978b4e0f774bb4e1cf4
mehul1011 <mehulshrivastava1011@gmail.com>
2021-04-13 13:14:19 +0530

    Added praticle.js animation variable which is imported inside the home component.

---
+++ src/AddOns/constants.js
@@ -0,0 +1,51 @@
+export const particlesOptions = {
+	"particles": {
+	        "number": {
+	            "value": 250,
+	            "density": {
+	                "enable": false
+	            }
+	        },
+	        "size": {
+	            "value": 5,
+	            "random": true,
+	            "anim": {
+	                "speed": 6,
+	                "size_min": 0.5
+	            }
+	        },
+	        "line_linked": {
+	            "enable": false
+	        },
+	        "move": {
+	            "random": true,
+	            "speed": 1,
+	            "direction": "bottom",
+	            "out_mode": "out"
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "bubble"
+	            },
+	            "onclick": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        },
+	        "modes": {
+	            "bubble": {
+	                "distance": 250,
+	                "duration": 2,
+	                "size": 0,
+	                "opacity": 0
+	            },
+	            "repulse": {
+	                "distance": 400,
+	                "duration": 4
+	            }
+	        }
+	    }
+};
