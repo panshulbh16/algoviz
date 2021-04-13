b5e0a76869b812333cf0a94ac024aa9238855691
Maestro1011 <m2makamaestro@gmail.com>
2021-04-13 13:19:35 +0530

    Added empty file for adding path finding algorithms in the future.

---
+++ src/PathFindingAlgorithm/pathFinding.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+export default class pathFinding extends React.Component{
+	// The constructor for a React component is called before it is mounted.
+	constructor(props){
+		// super is for calling the parent class (React.Component)
+		super(props);
+		// porps is passed, to access parent's attributes and use in this component
+		this.state = {};
+	}
+	render(){
+		return(
+      		
+			<div>
+				<h2 style={{marginTop: '50vh'}}>Path finding dalna he</h2>
+			</div>
+
+		);
+	}
+}
