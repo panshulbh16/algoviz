7a0195c08c6efa2fac161bae02e556cd88c4293f
Maestro1011 <m2makamaestro@gmail.com>
2021-04-13 19:59:18 +0530

    Trying to debug and change the format of returned animations array to the parent component.

--- src/SortingAlgorithms/MergeSort.js
+++ src/SortingAlgorithms/MergeSort.js
@@ -21,53 +21,28 @@ function mergeSortHelper(mainArray, startIdx, endIdx, auxiliaryArray, animations
 function doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations) {
     let k = startIdx, i = startIdx, j = middleIdx + 1;
     while (i <= middleIdx && j <= endIdx) {
-        animations.push([i, j]);
-        animations.push([i, j]);
+        animations.push(["compare1", i, j]);
+        animations.push(["compare2", i, j]);
         if (auxiliaryArray[i] <= auxiliaryArray[j]) {
-            animations.push([k, auxiliaryArray[i]]);
+            animations.push(["swap", k, auxiliaryArray[i]]);
             mainArray[k++] = auxiliaryArray[i++];
         }
         else {
-            animations.push([k, auxiliaryArray[j]]);
+            animations.push(["swap", k, auxiliaryArray[j]]);
             mainArray[k++] = auxiliaryArray[j++];
         }
     }
     while (i <= middleIdx) {
-        animations.push([i, i]);
-        animations.push([i, i]);
-        animations.push([k, auxiliaryArray[i]]);
+        animations.push(["compare1", i, i]);
+        animations.push(["comapre2", i, i]);
+        animations.push(["swap", k, auxiliaryArray[i]]);
         mainArray[k++] = auxiliaryArray[i++];
     }
     while (j <= endIdx) {
-        animations.push([j, j]);
-        animations.push([j, j]);
-        animations.push([k, auxiliaryArray[j]]);
+        animations.push(["compare1", j, j]);
+        animations.push(["comapre2", j, j]);
+        animations.push(["swap", k, auxiliaryArray[j]]);
         mainArray[k++] = auxiliaryArray[j++];
     }
 
 }
-
-/*
-export const mergeSort = array => {
-    if (array.length === 1)
-        return array;
-    const middleIdx = Math.floor(array.length / 2);
-    const firstHalf = mergeSort(array.slice(0, middleIdx));
-    const secondHalf = mergeSort(array.slice(middleIdx));
-    const sortedArray = [];
-    let i = 0, j = 0;
-    while (i < firstHalf.length && j < secondHalf.length) {
-        if (firstHalf[i] < secondHalf[j]) {
-            sortedArray.push(firstHalf[i++]);
-        }
-        else {
-            sortedArray.push(secondHalf[j++]);
-        }
-    }
-    while (i < firstHalf.length)
-        sortedArray.push(firstHalf[i++]);
-    while (j < secondHalf.length)
-        sortedArray.push(secondHalf[j++]);
-    return sortedArray;
-};
-*/
