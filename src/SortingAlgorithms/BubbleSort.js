10137c217ce318cc6ee3ec210bd3110245ae098a
Maestro1011 <m2makamaestro@gmail.com>
2021-04-13 19:58:45 +0530

    Trying to debug and change the format of returned animations array to the parent component.

--- src/SortingAlgorithms/BubbleSort.js
+++ src/SortingAlgorithms/BubbleSort.js
@@ -1,31 +1,32 @@
-// export function getBubbleSortAnimations(array)
-// {
-//     const animations = [];
-//     if (array.length <= 1)
-//         return array;
-//     const auxiliaryArray = array.slice();
-//     bubbleSort(auxiliaryArray, animations)
-//     return animations;
-// }
+export function getBubbleSortAnimations(array)
+{
+    const animations = [];
+    if (array.length <= 1)
+        return array;
+    const auxiliaryArray = array.slice();
+    bubbleSort(auxiliaryArray, animations)
+    return animations;
+}
 
-// function bubbleSort(auxiliaryArray, animations){
-// 	const n = auxiliaryArray.length;
-// 	let flag = false;
-// 	for (let i = 0; i<n-1; i++){
-// 		animations.push([i, i+1]);
-// 		animations.push([i, i+1]);
-// 		for (let j = 0; j<n-1; j++){
-// 			if (auxiliaryArray[j] > auxiliaryArray[j+1]){
-// 				flag = true;
-// 				animations.push([j, auxiliaryArray[j+1]]);
-// 				let temp = auxiliaryArray[i];
-// 				auxiliaryArray[j] = auxiliaryArray[j+1];
-// 				auxiliaryArray[j+1] = temp;
-// 		}
-// 	}
-// 	if (flag === false){
-// 		break;
-// 		}
+function bubbleSort(auxiliaryArray, animations){
+	const n = auxiliaryArray.length;
+	let flag = false;
+	for (let i = 0; i<n-1; i++){
+		for (let j = 0; j<n-1-i; j++){
+			animations.push(["compare1", j, j+1]);
+			animations.push(["compare2", j, j+1]);
+			if (auxiliaryArray[j] > auxiliaryArray[j+1]){
+				flag = true;
+				animations.push(["swap", j, auxiliaryArray[j+1]]);
+				animations.push(["swap", j+1, auxiliaryArray[j]]);
+				let temp = auxiliaryArray[j];
+				auxiliaryArray[j] = auxiliaryArray[j+1];
+				auxiliaryArray[j+1] = temp;
+		}
+	}
+	if (flag === false){
+		break;
+		}
 
-// 	}
-// }
+	}
+}
