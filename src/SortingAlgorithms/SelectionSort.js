12eb464a4750db0ab209104c5d3bc965232da474
Maestro1011 <m2makamaestro@gmail.com>
2021-04-13 19:57:43 +0530

    Added code for implementation of selection sort.

---
+++ src/SortingAlgorithms/SelectionSort.js
@@ -0,0 +1,28 @@
+export function getSelectionSortAnimations(array)
+{
+    const animations = [];
+    if (array.length <= 1)
+        return array;
+    const auxiliaryArray = array.slice();
+    selectionSort(auxiliaryArray, animations)
+    return animations;
+}
+
+function selectionSort(auxiliaryArray, animations){
+	const n = auxiliaryArray.length;
+	for (let i = 0; i<n-1; i++){
+		let minIndex = i;
+		for (let j = i+1; j<n; j++){
+			animations.push(["compare1", minIndex, j]);
+			animations.push(["compare2", minIndex, j]);
+			if (auxiliaryArray[minIndex] > auxiliaryArray[j]){
+				minIndex = j;
+			}
+		}
+		animations.push(["swap", minIndex, auxiliaryArray[i]]);
+		animations.push(["swap", i, auxiliaryArray[minIndex]]);
+		let temp = auxiliaryArray[minIndex];
+		auxiliaryArray[minIndex] = auxiliaryArray[i];
+		auxiliaryArray[i] = temp; 
+	}
+}
