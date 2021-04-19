ea49782d64479755597e65aa50300dbd4a441e52
Maestro1011 <m2makamaestro@gmail.com>
2021-04-19 23:57:54 +0530

    Added about us page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import './App.css';
 import Home from './MainPage/Home';
+import About from './About/About';
 import pathFinding from './PathFindingAlgorithm/pathFinding';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 import {particlesOptions} from './AddOns/constants';
 import Particles from 'react-particles-js';
 
+
 function App() {
   return (
   	<div className="App">
-  	  	<Router>
-    <div className="App">
-    	<Switch>
-    		<Route path="/" exact>
-    			<h1 className="home-fonts">AlgoViz</h1>
-    			<Home />
-    		</Route>
-    		<Route path="/sorting" component={SortingVisualizer} />
-    		<Route path="/pathfinding" component={pathFinding} />
-	    </Switch>
-    </div>
-    {
-    	// particle animation will only be in / or landing page
-    }
+    	<Router>
+      <div>
+      	<Switch>
       		<Route path="/" exact>
-  		<Particles className='particles' params={particlesOptions} />
-  		</Route>
-    </Router>
+      			<h1 className="home-fonts">AlgoViz</h1>
+      			<Home />
+      		</Route>
+      		<Route path="/sorting" component={SortingVisualizer} />
+      		<Route path="/pathfinding" component={pathFinding} />
+      		<Route path="/about" component={About} />
+  	    </Switch>
+      </div>
+      {
+      	// particle animation will only be in / or landing page
+      }
+        <Route path="/" exact>
+    		  <Particles className='particles' params={particlesOptions} />
+    		</Route>
+      </Router>
 
     {// if yor need particle animation in all pages un-commnet 
   		//<Particles className='particles' params={particlesOptions} />
