6895ee950a5a2dbafacb7adf015357204527e894
Maestro1011 <m2makamaestro@gmail.com>
2021-04-22 23:32:56 +0530

    Added close X button at the top of about page.

--- src/About/About.js
+++ src/About/About.js
@@ -21,7 +21,7 @@ export default class About extends React.Component{
       			<Cards />
       			<hr />
       			<Link to="/">
-	      			<Button className="btn btn-warning btn-lg sort-btn" type='submit'><h3>Visit Homepage</h3></Button>
+	      			<Button className="btn btn-warning about-btn" type='submit'><h3>X</h3></Button>
 	      		</Link>
 
 				<Link to="/sorting">
