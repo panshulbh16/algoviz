3a88d40f8e9de98d16ba85a3496ada85ddb5d652
Maestro1011 <m2makamaestro@gmail.com>
2021-04-28 13:31:06 +0530

    Added javascript dynamic object for making the project open source

---
+++ src/SortingVisualizer/allAlgorithms.js
@@ -0,0 +1,22 @@
+export const algo = [
+	{
+		"algoName": "NewArray",
+		"name": "Generate New Array",
+		"methods": "resetArray"
+	},
+	{	
+		"algoName": "Merge",
+		"name": "MergeSort",
+		"methods": "getMergeSortAnimations" 	
+	},
+	{
+		"algoName": "Bubble",
+		"name": "BubbleSort",
+		"methods": "getBubbleSortAnimations" 
+	},
+	{
+		"algoName": "Selection",
+		"name": "SelectionSort",
+		"methods": "getSelectionSortAnimations"
+	},
+];
