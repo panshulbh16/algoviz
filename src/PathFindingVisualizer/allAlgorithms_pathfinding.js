2473b66fd5389b595724a580701e3bc4b8eac384
parakhc4 <parakhc4@gmail.com>
2021-06-12 16:51:52 +0530

    Space/Time complexity updated

--- src/PathFindingVisualizer/allAlgorithms_pathfinding.js
+++ src/PathFindingVisualizer/allAlgorithms_pathfinding.js
@@ -10,7 +10,7 @@ export const algo = [
 		"id": 2,
 		"algoName": "Visualize Dijkstra",
 		"name":"Dijkstra",
-		"desc": "DIJKSTRA \nThe abstracted rules are as follows:\n1. Every time that we set out to visit a new node, we will choose the node with the smallest known distance/cost to visit first.\n2. Once we’ve moved to the node we’re going to visit, we will check each of its neighboring nodes.\n3. For each neighboring node, we’ll calculate the distance/cost for the neighboring nodes by summing the cost of the edges that lead to the node we’re checking from the starting vertex.\n4. Finally, if the distance/cost to a node is less than a known distance, we’ll update the shortest distance that we have on file for that vertex.\nTime Complexity: O(E Log V)where, E is the number of edges and V is the number of vertices."
+		"desc": "DIJKSTRA \nThe abstracted rules are as follows:\n1. Every time that we set out to visit a new node, we will choose the node with the smallest known distance/cost to visit first.\n2. Once we’ve moved to the node we’re going to visit, we will check each of its neighboring nodes.\n3. For each neighboring node, we’ll calculate the distance/cost for the neighboring nodes by summing the cost of the edges that lead to the node we’re checking from the starting vertex.\n4. Finally, if the distance/cost to a node is less than a known distance, we’ll update the shortest distance that we have on file for that vertex.\nTime Complexity: O(E Log V)where, E is the number of edges and V is the number of vertices.\nSpace Complexity: O(V)"
 	},
 	{
 		"id": 3,
@@ -28,7 +28,7 @@ export const algo = [
 		"id": 5,
 		"algoName": "Visualize DFS",
 		"name": "DFS",
-		"desc": "DEPTH FIRST SEARCH \nThe abstracted rules are as follows:\n1.In Depth-fist search we pick a starting node and push all its adjacent nodes into a stack.\n2.Then pop a node from stack to select the next node to visit and push all its adjacent nodes into a stack.\n3.Repeat this process until the stack is empty. However, ensure that the nodes that are visited are marked. This will prevent you from visiting the same node more than once. If you do not mark the nodes that are visited and you visit the same node more than once, you may end up in an infinite loop.\nTime complexity: O(V+E)where V is the number of vertices in the graph and E is the number of edges in the graph, when implemented using an adjacency list.\nSpace Complexity: O(H) where H is the maximum height of the tree."
+		"desc": "DEPTH FIRST SEARCH \nThe abstracted rules are as follows:\n1.In Depth-fist search we pick a starting node and push all its adjacent nodes into a stack.\n2.Then pop a node from stack to select the next node to visit and push all its adjacent nodes into a stack.\n3.Repeat this process until the stack is empty. However, ensure that the nodes that are visited are marked. This will prevent you from visiting the same node more than once. If you do not mark the nodes that are visited and you visit the same node more than once, you may end up in an infinite loop.\nTime complexity: O(V+E)where V is the number of vertices in the graph and E is the number of edges in the graph, when implemented using an adjacency list.\nSpace Complexity: O(|V|)."
 	},
 ];
 
